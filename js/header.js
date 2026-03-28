37880c31ceaf150a3d3e699468826c6cf6ddec18
ЗЗСО І-ІІІ ст. № 9 <toretsk098@gmail.com>
2026-03-28 18:24:15 +0200

    Update html and js, add html file
    1) Додано таблиці з графіками на тиждень з 30.03.2026 до 03.04.2026 р. 유아
    2) Додано сторінку "Квітень-2026".
    3) Оновлено меню в js.

--- js/header.js
+++ js/header.js
@@ -128,6 +128,7 @@ const headerHTML = `
                         <li class="menu-children">
                             <a href="#">Новини<br>закладу</a>
                             <ul>
+                                <li><a href="./2026-04-april.html">Квітень-2026</a></li>
                                 <li><a href="./2026-03-march.html">Березень-2026</a></li>
                                 <li><a href="./2026-02-february.html">Лютий-2026</a></li>
                                 <li><a href="./2026-01-january.html">Січень-2026</a></li>
@@ -223,6 +224,7 @@ const headerHTML = `
                                 <li class="menu-children">
                                     <a href="#">Новини закладу</a>
                                     <ul class="list main-menu-mobile main-submenu-mobile">
+                                        <li><a href="./2026-04-april.html">Квітень-2026</a></li>
                                         <li><a href="./2026-03-march.html">Березень-2026</a></li>
                                         <li><a href="./2026-02-february.html">Лютий-2026</a></li>
                                         <li><a href="./2026-01-january.html">Січень-2026</a></li>
